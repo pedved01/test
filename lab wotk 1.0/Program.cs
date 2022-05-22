cccb86720f2f405e40edeb03b17d540637873550
pedved <pedved@DESKTOP-DG6R9KV>
2022-05-22 22:03:33 +0300

    Добавьте файлы проекта.

---
+++ lab wotk 1.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_stud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*  Console.WriteLine("Hello Debug!");
+              Console.WriteLine("Hello Read!");
+              System.Console.WriteLine("Hello World000!");*/
+            Console.ReadLine();
+        }
+    }
+}
